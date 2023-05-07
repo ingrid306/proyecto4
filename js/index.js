e978fd532a6c97d8fdf8f98cdbd03bead4e7f2ca
ingrid <inpaito00@gmail.com>
2023-05-07 16:25:41 -0300

    proyecto4

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//OPERACIONES BÁSICAS
+let n1 = 2
+let n2 = 4
+let n3 = 6
+
+//suma
+let resultadosuma = n1 + n2
+console.log (resultadosuma)
+
+//RESTA
+let resultadoresta = n2 - n1
+console.log (resultadoresta)
+
+//MULTIPLICACIÓN
+let resultadomult = n3 + n1
+console.log (resultadomult)
+
+//DIVISIÓN
+let resultadodiv = n3 + n2
+console.log (resultadodiv)
